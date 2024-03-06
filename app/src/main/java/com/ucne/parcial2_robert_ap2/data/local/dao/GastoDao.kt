8bc302f64686e4275ee2b58336d08e3e8faf6d4b
Robert Tejada <robert_tejada@ucne.edu.do>
2024-03-06 19:52:13 -0400

    Agregando el SingletonComponent al InstallIn y Eliminando el Suspend al ObtenerTodos en el Dao

--- app/src/main/java/com/ucne/parcial2_robert_ap2/data/local/dao/GastoDao.kt
+++ app/src/main/java/com/ucne/parcial2_robert_ap2/data/local/dao/GastoDao.kt
@@ -1,21 +1,23 @@
 package com.ucne.parcial2_robert_ap2.data.local.dao
 
+import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Query
 import androidx.room.Upsert
 import com.ucne.parcial2_robert_ap2.data.local.entities.GastoEntity
 import kotlinx.coroutines.flow.Flow
 
+@Dao
 interface GastoDao {
     @Upsert
     suspend fun Guardar(gasto: GastoEntity)
 
-    @Query("SELECT * FROM gastos WHERE gastoId = :id LIMIT 1")
+    @Query("SELECT * FROM gastos WHERE gastoId=:id LIMIT 1")
     suspend fun ObtenerPorId(id: Int): GastoEntity?
 
     @Delete
     suspend fun Eliminar(gasto: GastoEntity)
 
     @Query("SELECT * FROM gastos")
-    suspend fun ObtenerTodos(): Flow<List<GastoEntity>>
+    fun ObtenerTodos(): Flow<List<GastoEntity>>
 }
