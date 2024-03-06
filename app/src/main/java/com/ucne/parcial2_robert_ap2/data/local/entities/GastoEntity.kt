3a3e320194162930398347e9e958343f6adbbf46
Robert Tejada <robert_tejada@ucne.edu.do>
2024-03-06 18:30:03 -0400

    Subiendo la entidad de gastos

---
+++ app/src/main/java/com/ucne/parcial2_robert_ap2/data/local/entities/GastoEntity.kt
@@ -0,0 +1,17 @@
+package com.ucne.parcial2_robert_ap2.data.local.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "gastos")
+data class GastoEntity(
+    @PrimaryKey(autoGenerate = true)
+    val gastoId: Int = 0,
+    val fecha: String = "",
+    val suplidor: String = "",
+    val ncf: String = "",
+    val concepto: String = "",
+    val descuento: String = "",
+    val itbis: Int = 0,
+    val monto: Int = 0
+)
